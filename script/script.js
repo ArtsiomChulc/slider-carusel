339e7a02eaea0b62b2736a2ff0d5e192815e5173
Artsiom <artemorsha@mail.ru>
2023-01-30 14:29:41 +0300

    ref script

--- script/script.js
+++ script/script.js
@@ -73,55 +73,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	});
 
-
-
-
-
-
-
-	// showSlides(slideIndex);
-
-	// if (slides.length < 10) {
-	// 	total.textContent = `0${slides.length}`;
-	// } else {
-	// 	total.textContent = slides.length;
-	// }
-
-	// function showSlides(n) {
-	// if (n > slides.length) {
-	// 	slideIndex = 1;
-	// 	}
-	// 	if (n < 1) {
-	// 		slideIndex = slides.length;
-	// 	}
-	// 	slides.forEach(slide => {
-	// 		slide.style.display = 'none';
-	// 	});
-	// 	slides[slideIndex - 1].style.display = 'block';
-
-	// if (slideIndex < 10) {
-	// 	current.textContent = `0${slideIndex}`;
-	// } else {
-	// 	current.textContent = slideIndex;
-	// }
-	// }
-
-	// function plusSlide(e) {
-	// 	showSlides(slideIndex += e);
-	// }
-
-	// prevBtn.addEventListener('click', () => {
-	// 	plusSlide(-1);
-	// });
-	// nextBtn.addEventListener('click', () => {
-	// 	plusSlide(1);
-	// });
-
-
-
-
-
-
 });
 
 
